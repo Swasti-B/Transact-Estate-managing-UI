3d39452f9968f616c654c88317a568dbc5c7daf2
Swastee <baralswasti@gmail.com>
2021-01-26 15:07:40 -0600

    Sign-in page

---
+++ src/Components/SignIn.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './SignIn.css'
+
+function SignIn()
+{
+return(
+    <div className= "page">
+    <div className= "block">
+     </div>
+    <img className = "logo-img" src = "/Images/trans.png" alt= "transact-logo" />
+
+    <h1>WELCOME TO TRANSACT ESTATE MANAGING SERVICE!</h1>
+    <div className ="sign-in">
+        <button className ="btn"> Sign in</button>
+    </div>
+    <div className= "footer">    
+    </div>
+    </div>
+)
+
+}
+
+export default SignIn
+
